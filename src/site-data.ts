b983a1104dde195456a212de45b0fd7b3a61bb01
njthomson <njthomson@outlook.com>
2025-10-20 18:23:03 -0700

    - Handle null-ref when matching types by buildType

--- src/site-data.ts
+++ src/site-data.ts
@@ -188,8 +188,8 @@ export const isOrbital = (buildType: string | undefined): boolean => {
 
 export const getSiteType = (buildType: string, noThrow?: boolean): SiteType | undefined => {
   if (noThrow && !buildType) { buildType = '' };
-  const match = siteTypes.find(st => st.subTypes.includes(buildType) || st.altTypes?.includes(buildType) || st.subTypes.includes(buildType.slice(0, -1)));
-  if (!match) {
+  const match = siteTypes.find(st => st.subTypes.includes(buildType) || st.altTypes?.includes(buildType) || st.subTypes.includes(buildType?.slice(0, -1)));
+  if (!match && buildType) {
     console.error(`No SiteType match found for: '${buildType}'`);
     return undefined;
   }
